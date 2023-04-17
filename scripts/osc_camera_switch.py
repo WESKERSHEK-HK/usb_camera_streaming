19c5fa0af8d0777a98a2db16c4ad2180da408157
weskerwork@gmail.com <weskerwork@gmail.com>
2023-04-18 00:08:14 +0800

    update

--- scripts/osc_camera_switch.py
+++ scripts/osc_camera_switch.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+import subprocess
 from pythonosc import dispatcher
 from pythonosc import osc_server
-import rospy
-from std_srvs.srv import Empty
 
 def switch_cameras_handler(unused_addr, args):
     cam1, cam2 = args
-    rospy.wait_for_service('/{}/switch_cameras'.format(cam1))
-    rospy.wait_for_service('/{}/switch_cameras'.format(cam2))
     try:
-        switch_cam1 = rospy.ServiceProxy('/{}/switch_cameras'.format(cam1), Empty)
-        switch_cam2 = rospy.ServiceProxy('/{}/switch_cameras'.format(cam2), Empty)
-        switch_cam1()
-        switch_cam2()
-    except rospy.ServiceException as e:
+        subprocess.check_call(['rosservice', 'call', '/{}/switch_cameras'.format(cam1)])
+        subprocess.check_call(['rosservice', 'call', '/{}/switch_cameras'.format(cam2)])
+    except subprocess.CalledProcessError as e:
         print("Service call failed: %s" % e)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="192.168.1.131", help="The IP to listen on")
+    parser.add_argument("--ip", default="192.168.1.131, help="The IP to listen on")
     parser.add_argument("--port", type=int, default=20000, help="The port to listen on")
     parser.add_argument("--cam1", default="usb_cam1", help="The first camera node name")
     parser.add_argument("--cam2", default="usb_cam2", help="The second camera node name")
     args = parser.parse_args()
-
-    rospy.init_node('osc_camera_switch')
 
     disp = dispatcher.Dispatcher()
     disp.map("/switch_cameras", switch_cameras_handler, args.cam1, args.cam2)
